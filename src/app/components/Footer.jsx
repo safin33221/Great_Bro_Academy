6eaec4a1628ec689d0c1a7d2d0d2568a6b4d2d4d
safin33221 <safin33221@gmail.com>
2025-07-06 22:09:46 +0600

    Update Footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -14,11 +14,13 @@ const Footer = () => {
 
                 <div>
 
-                    <Image src={logo} width={100} height={100} alt="logo" className="block dark:hidden" />
-                    <Image src={DarkLogo} width={100} height={100} alt="logo" className="hidden dark:block" />
-                    <p className="mt-2 text-sm text-muted-foreground dark:text-orange-400">
-                        Empowering learners with practical IT skills.
-                    </p>
+                    <Image src={logo} width={150} height={150} alt="logo" className="block dark:hidden" />
+                    <Image src={DarkLogo} width={150} height={150} alt="logo" className="hidden dark:block" />
+                    <address>
+                        House-2/C,Ground Floor, Road-13, <br />
+                        (Beside Lotus Kamal Tower) <br />
+                        Nikunjo-2,Khilkhet,Dhaka-1229
+                    </address>
                 </div>
 
 
@@ -37,10 +39,10 @@ const Footer = () => {
                 <div>
                     <h3 className="text-md font-semibold mb-2">Popular Courses</h3>
                     <ul className="space-y-1 text-sm">
-                        <li><Link href="/courses/graphic-design" className="hover:underline">Graphic Design</Link></li>
-                        <li><Link href="/courses/web-development" className="hover:underline">Web Development</Link></li>
+                        <li><Link href="/courses/graphic-design-course" className="hover:underline">Graphic Design</Link></li>
+                        <li><Link href="/courses/web-development-bootcamp" className="hover:underline">Web Development</Link></li>
                         <li><Link href="/courses/ui-ux" className="hover:underline">UI/UX Design</Link></li>
-                        <li><Link href="/courses/video-editing" className="hover:underline">Video Editing</Link></li>
+                        <li><Link href="/courses/spoken-english-course" className="hover:underline">Spoken English</Link></li>
                     </ul>
                 </div>
 
@@ -48,9 +50,9 @@ const Footer = () => {
                 <div>
                     <h3 className="text-md font-semibold mb-2">Follow Us</h3>
                     <ul className="space-y-1 text-sm">
-                        <li><Link href="#" className="hover:underline">Facebook</Link></li>
+                        <li><Link href="https://www.facebook.com/greatbroacademy" target="_blank" className="hover:underline">Facebook</Link></li>
                         <li><Link href="#" className="hover:underline">YouTube</Link></li>
-                        <li><Link href="#" className="hover:underline">LinkedIn</Link></li>
+                        <li><Link href="https://www.linkedin.com/company/greatbro-academy" target="_blank" className="hover:underline">LinkedIn</Link></li>
                     </ul>
                 </div>
             </div>
